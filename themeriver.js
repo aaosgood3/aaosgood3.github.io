4f98c20a9e3ae872244513dacaef1d7ecf30da31
Allen Osgood <aaosgood3@gmail.com>
2016-11-15 16:15:33 -0600

    23

--- themeriver.js
+++ themeriver.js
@@ -1,15 +1,18 @@
 var dataUri = "data.csv";
 //var colors = [new RGBColour(108,209,230), new RGBColour(224,227,204),new RGBColour(250,104,0)];
 var colors = d3.scale.category20();
+var data = [];
 var graphColors = [];
 var years = [];
 
+var margin = {top: 20, right: 40, bottom: 30, left: 30};
+var width = document.body.clientWidth - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
+
 buildChart(dataUri);
+addToolTip();
 
 function buildChart(uri) {
-	var margin = {top: 20, right: 40, bottom: 30, left: 30};
-	var width = document.body.clientWidth - margin.left - margin.right;
-	var height = 400 - margin.top - margin.bottom;
 
 	var strokeColor = "#09101c";//randomColorMix(colors[0].getRGB(), colors[1].getRGB(), colors[2].getRGB(), 1);
 
@@ -38,6 +41,9 @@ function buildChart(uri) {
 				return {x: c, y: +d[c]};
 			});
 		}));
+
+		// Make the data global
+		data = layers;
 
 		x.domain(d3.extent(layers[0], function(d) { return d.x; }));
 		y.domain([0, d3.max(layers[layers.length - 1], function(d) { return d.y0 + d.y; })]);
@@ -72,28 +78,79 @@ function buildChart(uri) {
 		svg.append("g")
 		.attr("class", "y axis")
 		.call(yAxisLeft);
+
+		svg.selectAll(".layer")
+		.attr("opacity", "0.2")
+		.on("mouseover", toolTipMouseOver)
+		.on("mouseout", toolTipMouseOut);
 	});
+
+	function toolTipMouseOver(d, i) {
+		d3.select(this).attr({
+			opacity: 1,
+			fill: "#F3F315",
+			stroke: strokeColor,
+			stroke-width: "1px"
+		});
+
+		addToolTip(d, i);
+	}
+
+	function toolTipMouseOut(d, i) {
+		d3.selectAll(".layer").attr("opacity", 1);
+
+		d3.select(this).attr({
+			fill: colors(i),
+			stroke-width: "0px"
+		});
+
+		removeToolTip();
+	}
+
+	function addToolTip(d, i) {
+		console.log(d);
+		var mouseXY = d3.mouse(this);
+
+		d3.select("#tooltip")
+		.attr("top", mouseXY[1] - 10)
+		.attr("left", mouseXY[0] + 10)
+		.attr("right", width - margin.right)
+		.html("(" + d.key + ", " + d.x + ", " + d.y + ")");
+	}
+
+	function removeToolTip() {
+		d3.select("#tooltip").attr("display", "none");
+	}
 }
 
-function randomColorMix(color1, color2, color3, greyControl) {
-	var randomIndex = getRandomInt(0, 2);
-
-	var mixRatio1 = (randomIndex == 0) ? Math.random() * greyControl : Math.random();
-	var mixRatio2 = (randomIndex == 1) ? Math.random() * greyControl : Math.random();
-	var mixRatio3 = (randomIndex == 2) ? Math.random() * greyControl : Math.random();
-
-	var sum = mixRatio1 + mixRatio2 + mixRatio3;
-
-	mixRatio1 /= sum;
-	mixRatio2 /= sum;
-	mixRatio3 /= sum;
-
-	var r = mixRatio1 * color1.r + mixRatio2 * color2.r + mixRatio3 * color3.r;
-	var g = mixRatio1 * color1.g + mixRatio2 * color2.g + mixRatio3 * color3.g;
-	var b = mixRatio1 * color1.b + mixRatio2 * color2.b + mixRatio3 * color3.b;
-
-	return new RGBColour(r, g, b);
+function createToolTip() {
+	var tooltip = d3.select("div#graph").append("div")
+	.attr("id", "tooltip")
+	.attr("position", "absolute")
+	.attr("top","0px")
+	.attr("left", "0px")
+	.attr("display","none");
 }
+
+// function randomColorMix(color1, color2, color3, greyControl) {
+// 	var randomIndex = getRandomInt(0, 2);
+
+// 	var mixRatio1 = (randomIndex == 0) ? Math.random() * greyControl : Math.random();
+// 	var mixRatio2 = (randomIndex == 1) ? Math.random() * greyControl : Math.random();
+// 	var mixRatio3 = (randomIndex == 2) ? Math.random() * greyControl : Math.random();
+
+// 	var sum = mixRatio1 + mixRatio2 + mixRatio3;
+
+// 	mixRatio1 /= sum;
+// 	mixRatio2 /= sum;
+// 	mixRatio3 /= sum;
+
+// 	var r = mixRatio1 * color1.r + mixRatio2 * color2.r + mixRatio3 * color3.r;
+// 	var g = mixRatio1 * color1.g + mixRatio2 * color2.g + mixRatio3 * color3.g;
+// 	var b = mixRatio1 * color1.b + mixRatio2 * color2.b + mixRatio3 * color3.b;
+
+// 	return new RGBColour(r, g, b);
+// }
 
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
