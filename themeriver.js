c3af0c79d250dce46689a30c64796204478281f7
Allen Osgood <aaosgood3@gmail.com>
2016-11-15 09:05:49 -0600

    Attempt 1

--- themeriver.js
+++ themeriver.js
@@ -1,11 +1,12 @@
 var dataUri = "data.csv";
 var colors = [new RGBColour(108,209,230), new RGBColour(224,227,204),new RGBColour(250,104,0)];
+var graphColors = [];
 var years = [];
 
 buildChart(dataUri);
 
 function buildChart(uri) {
-	var margin = {top: 30, right: 30, bottom: 30, left: 30};
+	var margin = {top: 20, right: 20, bottom: 40, left: 50}
 	var width = document.body.clientWidth - margin.left - margin.right;
 	var height = 400 - margin.top - margin.bottom;
 
@@ -14,31 +15,12 @@ function buildChart(uri) {
 
 	var format = d3.time.format("%Y");
 
-	var x = d3.time.scale().range([0, width]);
-	var y = d3.scale.linear().range([height-10, 0]);
+	var x = d3.scale.ordinal().range([0, width]);
+	var y = d3.scale.linear().range([height, 0]);
 	
-	//var colors = d3.scale.ordinal().range(colorrange);
-
 	var xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(d3.time.years);
-	var yAxisLeft = d3.svg.axis().scale(y);
-	var yAxisRight = d3.svg.axis().scale(y);
-
-	var stack = d3.layout.stack()
-	.offset("silhouette")
-	.values(function(d) { 
-		console.log(d);
-		return d.values; })
-	.x(function(d) { return d.date; })
-	.y(function(d) { return d.value; });
-
-	var nest = d3.nest()
-	.key(function(d) { return d.key; });
-
-	var area = d3.svg.area()
-	.interpolate("cardinal")
-	.x(function(d) { return x(d.date); })
-	.y0(function(d) { return y(d.y0); })
-	.y1(function(d) { return y(d.y0 + d.y); });
+	var yAxisLeft = d3.svg.axis().scale(y).orient("left");
+	var yAxisRight = d3.svg.axis().scale(y).orient("right");
 
 	var svg = d3.select(".graph").append("svg")
 	.attr("width", width + margin.left + margin.right)
@@ -46,16 +28,55 @@ function buildChart(uri) {
 	.append("g")
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	var graph = d3.csv(uri, function(data) {
+	d3.csv(uri, function(data) {
 		console.log(data);
-		data.forEach(function(d) {
-			console.log(d);
-		});
+		var headers = d3.keys(data[0]);
+		console.log(headers);
 
-		var layers = stack(nest.entries(data));
+		// Remove League
+		headers.splice(headers.indexOf('League'), 1);
+		console.log(headers);
 
-		x.domain(d3.extent(data, function(d) { return d.date; }));
-		y.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
+		var layers = d3.layout.stack().offset("silhouette")(data.map(function(d) {
+			return headers.map(function(c) {
+				d.key = d.League;
+				return {x: c, y: +d[c]};
+			});
+		}));
+
+		x.domain(layers[0].map(function(d) { return d.x; }));
+		y.domain([0, d3.max(layers[layers.length - 1], function(d) { return d.y0 + d.y; })]);
+
+		while (graphColors.length < headers.length) {
+			graphColors.push(randomColorMix(colors[0].getRGB(), colors[1].getRGB(), colors[2].getRGB(), 1));
+		}
+
+		var layer = svg.selectAll(".layer")
+		.data(layers)
+		.enter().append("path")
+		.attr("class", "layer")
+		.attr("d", function(d) { return area(d.values); });
+		.style("fill", function(d, i) { return graphColors(i); });
+
+		var area = svg.area()
+		.interpolate("cardinal")
+		.x(function(d) { return x(d.x); })
+		.y0(function(d) { return y(d.y0); })
+		.y1(function(d) { return y(d.y0 + d.y); });
+
+		svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+
+		svg.append("g")
+		.attr("class", "y axis")
+		.attr("transform", "translate(" + width + ", 0)")
+		.call(yAxisRight);
+
+		svg.append("g")
+		.attr("class", "y axis")
+		.call(yAxisLeft);
 	});
 }
 
