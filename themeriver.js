b8d4efdd1bc45d927c30798cea9a4c280162520d
Allen Osgood <aaosgood3@gmail.com>
2016-11-16 10:43:09 -0600

    28

--- themeriver.js
+++ themeriver.js
@@ -149,30 +149,6 @@ function createToolTip() {
 	.style("display","none");
 }
 
-// function randomColorMix(color1, color2, color3, greyControl) {
-// 	var randomIndex = getRandomInt(0, 2);
-
-// 	var mixRatio1 = (randomIndex == 0) ? Math.random() * greyControl : Math.random();
-// 	var mixRatio2 = (randomIndex == 1) ? Math.random() * greyControl : Math.random();
-// 	var mixRatio3 = (randomIndex == 2) ? Math.random() * greyControl : Math.random();
-
-// 	var sum = mixRatio1 + mixRatio2 + mixRatio3;
-
-// 	mixRatio1 /= sum;
-// 	mixRatio2 /= sum;
-// 	mixRatio3 /= sum;
-
-// 	var r = mixRatio1 * color1.r + mixRatio2 * color2.r + mixRatio3 * color3.r;
-// 	var g = mixRatio1 * color1.g + mixRatio2 * color2.g + mixRatio3 * color3.g;
-// 	var b = mixRatio1 * color1.b + mixRatio2 * color2.b + mixRatio3 * color3.b;
-
-// 	return new RGBColour(r, g, b);
-// }
-
-// function getRandomInt(min, max) {
-// 	return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
 function fileSelected() {
 	var file = document.getElementById('fileToUpload').files[0];
 	if (file) {
@@ -205,6 +181,7 @@ function uploadFile() {
 
 function invertData() {
 	invertedLayers = !invertedLayers;
+	document.getElementById("graph").innerHTML = "";
 	if (usingDefaultData) {
 		getCSVData(dataUri);
 	} else {
