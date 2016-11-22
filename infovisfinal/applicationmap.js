d1309d5d2d97bddfad896744bb262559ca5bf1a1
Allen Osgood <aaosgood3@gmail.com>
2016-11-21 21:03:31 -0600

    Added data

--- infovisfinal/applicationmap.js
+++ infovisfinal/applicationmap.js
@@ -14,8 +14,22 @@ var path = d3.geo.path()
 
 var g = svg.append("g");
 
-// load and display the World
 d3.json("world-110m2.json", function(error, topology) {
+	d3.csv("data.csv", function(data) {
+		g.selectAll("circle")
+			.data(data)
+			.enter()
+			.append("circle")
+			.attr("cx", function(d) {
+				return projection([d.Lng, d.Lat][0]);
+			})
+			.attr("cy", function(d) {
+				return projection([d.Lng, d.Lat][1]);
+			})
+			.attr("r", 3)
+			.style("fill", "black");
+	});
+
     g.selectAll("path")
       .data(topojson.object(topology, topology.objects.countries)
           .geometries)
@@ -23,3 +37,15 @@ d3.json("world-110m2.json", function(error, topology) {
       .append("path")
       .attr("d", path)
 });
+
+var zoom = d3.behavior.zoom()
+    .on("zoom",function() {
+        g.attr("transform","translate("+ 
+            d3.event.translate.join(",")+")scale("+d3.event.scale+")");
+        g.selectAll("circle")
+            .attr("d", path.projection(projection));
+        g.selectAll("path")  
+            .attr("d", path.projection(projection)); 
+  });
+
+svg.call(zoom);
