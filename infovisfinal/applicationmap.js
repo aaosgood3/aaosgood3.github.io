a831f31c387268ab5a4ddc8be8e0cf0cd8ef9e23
Allen Osgood <aaosgood3@gmail.com>
2016-11-21 21:14:06 -0600

    Fixed order of ops

--- infovisfinal/applicationmap.js
+++ infovisfinal/applicationmap.js
@@ -21,11 +21,10 @@ d3.json("world-110m2.json", function(error, topology) {
 			.enter()
 			.append("circle")
 			.attr("cx", function(d) {
-				console.log("lat: " + d.Lat + ", lng: " + d.Lng);
-				return projection([d.Lng, d.Lat][0]);
+				return projection([d.Lng, d.Lat])[0];
 			})
 			.attr("cy", function(d) {
-				return projection([d.Lng, d.Lat][1]);
+				return projection([d.Lng, d.Lat])[1];
 			})
 			.attr("r", 3)
 			.style("fill", "black");
