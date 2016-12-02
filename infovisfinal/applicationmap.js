3d4e963e1b8afb845219f6f33d1dbdf54c1aa0dd
Allen Osgood <aaosgood3@gmail.com>
2016-12-02 00:19:32 -0600

    First timeline attempt

--- infovisfinal/applicationmap.js
+++ infovisfinal/applicationmap.js
@@ -1,14 +1,17 @@
-
 var width = 960,
     height = 500;
 
 var projection = d3.geo.mercator()
-    .center([0, 5 ])
-    .rotate([-180,0]);
+    .center([0, 5])
+    .rotate([-180, 0]);
 
 var svg = d3.select("#graph").append("svg")
     .attr("width", width)
     .attr("height", height);
+
+var slider = d3.select("#graph").append("div")
+	.attr("width", width)
+	.style("margin", "20px 0 10px 20px");
 
 var tip = d3.tip()
 	.attr('class', 'd3-tip')
@@ -20,33 +23,24 @@ var tip = d3.tip()
 
 svg.call(tip);
 
-var path = d3.geo.path()
-    .projection(projection);
+var path = d3.geo.path().projection(projection);
 
 var g = svg.append("g");
 
 d3.json("world-110m2.json", function(error, topology) {
-	d3.csv("data.csv", function(data) {
-		minDate = data[0].Time;
-		maxDate = data[data.length-1].Time;
-
-		g.selectAll("circle")
-			.data(data)
-			.enter()
-			.append("circle")
-			.attr("cx", function(d) {
-				return projection([d.Lng, d.Lat])[0];
-			})
-			.attr("cy", function(d) {
-				return projection([d.Lng, d.Lat])[1];
-			})
-			.attr("r", 1)
-			.style("fill", "white")
-			.style("border", "#8c3752")
-			.style("fill-opacity","0.0")
-			.on('mouseover', tip.show)
-      		.on('mouseout', tip.hide);
-	});
+	d3.csv("data.csv")
+		.row(function(d) {
+			return {
+				university: d.University,
+				lat: parseFloat(d.Lat),
+				lng: parseFloat(d.Lng),
+				time: moment(d.Time, "YYYY-MM-DD HH:mm:ss").unix()
+			};
+		})
+		.get(function(err, rows) {
+    		if (err) return console.error(err);
+    		window.site_data = rows;
+    	});
 
     g.selectAll("path")
       .data(topojson.object(topology, topology.objects.countries)
@@ -68,26 +62,53 @@ var zoom = d3.behavior.zoom()
 
 svg.call(zoom);
 
-/**
- * Window-scale functions
- */
+var displaySites = function(data) {
+  var sites = svg.selectAll(".site")
+      .data(data, function(d) {
+        return d.university;
+      });
 
-// Zoom on window resize
-var parent = d3.select("#graph");
+  sites.enter().append("circle")
+      .attr("class", "site")
+      .attr("cx", function(d) {
+        return projection([d.lng, d.lat])[0];
+      })
+      .attr("cy", function(d) {
+        return projection([d.lng, d.lat])[1];
+      })
+      .attr("r", 1)
+      .transition().duration(400)
+  	  	.attr("r", 5)
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide);
 
-d3.select(window)
-	.on('resize', function() {
-		var width = parent.width();
-		var height = parent.height();
+  sites.exit()
+    .transition().duration(200)
+      .attr("r",1)
+      .remove();
+};
 
-		// Set the map's width and height
-		svg.attr('width', width);
-		svg.attr('height', height);
-		map.setWidth(width);
-		map.setHeight(height);
+var minDateUnix = moment(data[0].Time, "YYYY MM DD").unix();
+var maxDateUnix = moment(data[data.length-1].Time, "YYYY MM DD").unix();
+var secondsInDay = 60 * 60 * 24;
 
-		// Set the timeline's width and height
-		svgTimeline.attr('width', width);
-		timeline.setWidth(width);
-	});
+var updateData = d3.slider()
+  .axis(true).min(minDateUnix).max(maxDateUnix).step(secondsInDay)
+  .on("slide", function(evt, value) {
+    var newData = _(site_data).filter( function(site) {
+      return site.time < value;
+    })
+    displaySites(newData);
+  });
 
+d3.select('#slider').call(updateData);
+
+// Resize with window size change
+$(window).resize(function(){
+	document.getElementById("graph").innerHTML = "";
+	if (usingDefaultData) {
+		getCSVData(dataUri);
+	} else {
+		parseCSVData(lastUploadedCSV);
+	}
+});
