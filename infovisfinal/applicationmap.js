f557d53a3d4fe0c4ed1b1babfcad54d7e33c1e23
Allen Osgood <aaosgood3@gmail.com>
2016-12-06 17:56:35 -0600

    35

--- infovisfinal/applicationmap.js
+++ infovisfinal/applicationmap.js
@@ -9,8 +9,14 @@ function createMap() {
 	.rotate([-180, 0]);
 
 	var svg = d3.select("#graph").append("svg")
-	.attr("width", width)
-	.attr("height", height);
+	.append("div")
+    .classed("svg-container", true) //container class to make it responsive
+    .append("svg")
+    //responsive SVG needs these 2 attributes and no width and height attr
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 600 400")
+    //class to make it responsive
+    .classed("svg-content-responsive", true); 
 
 	var slider = d3.select("#graph").append("div")
 	.attr("id", "slider")
